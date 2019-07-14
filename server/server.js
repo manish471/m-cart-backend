34a12f9a215baf5698b91fc5196666d4daa818d3
Manish Chougule <manish@rxlab.solutions>
2019-07-15 00:05:13 +0530

    production config removed and Procfile added

--- server/server.js
+++ server/server.js
@@ -249,11 +249,3 @@ const port = process.env.PORT || 3002;
 app.listen(port,()=>{
     console.log(`server running at ${port}`);
 })
-
-if(process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('client/build'));
-}
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
